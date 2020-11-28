776afe82b8c3f8f9db7bef2784c7313ae94ccc3c
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2020-11-28 10:31:47 -0300

    2020-11-28

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { useRouter } from "next/router";
+
+import Navigation from "./Navigation";
+import Logo from "./Logo";
+import { LanguageContext, locales } from "../intl/LanguageProvider";
+
+interface Props {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+const Header: React.FC<Props> = ({ className, children }) => {
+    const headerClass = className || "header";
+    const [locale, setLocale] = useContext(LanguageContext);
+    const router = useRouter();
+
+    function handleLocaleChange(language: string) {
+        const regex = new RegExp(`^/(${locales.join("|")})`);
+        setLocale(language);
+
+        router.push(
+            router.pathname,
+            router.asPath.replace(regex, `/${language}`)
+        );
+    }
+
+    return (
+        <header className={headerClass}>
+            <Logo link={`/`} />
+            <Navigation />
+            {children}
+            <div className="lang">
+                <button onClick={() => handleLocaleChange("en")}>EN</button>
+                <button onClick={() => handleLocaleChange("pt")}>PT</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/Navigation.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+
+import useTranslation from "../intl/useTranslation";
+
+interface Props {
+    className?: string;
+}
+
+const Navigation: React.FC<Props> = ({ className }) => {
+    const { t, locale } = useTranslation();
+    const navClass = className || "navigation";
+
+    return (
+        <nav className={navClass}>
+            <ul>
+                <li>
+                    <Link href={`/`} as={`/`}>
+                        <a>{t("home")}</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href={`/[lang]/articles`} as={`/${locale}/articles`}>
+                        <a>{t("articles")}</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href={`/about`} as={`/about`}>
+                        <a>{t("about")}</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
