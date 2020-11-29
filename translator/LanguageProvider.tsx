59b301c44a6ba295e699ba597badae1f5a3fee15
zherar7ordoya <zherar7ordoya@outlook.com>
2020-11-29 01:13:46 -0300

    2020-11-29

--- translator/LanguageProvider.tsx
+++ translator/LanguageProvider.tsx
@@ -5,7 +5,7 @@ export const locales = ["pt", "en"];
 export const LanguageContext = createContext([]);
 
 export const LanguageProvider: React.FC = ({ children }) => {
-    const [locale, setLocale] = useState("pt");
+    const [locale, setLocale] = useState("en");
 
     return (
         <LanguageContext.Provider value={[locale, setLocale]}>
